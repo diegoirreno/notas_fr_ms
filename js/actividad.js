b051501bbd775b52bf5debe31140789b012d9e0e
diegoirreno <dfirreno@uniboyaca.edu.co>
2023-06-03 18:20:46 -0500

    Se creo actividad.js para implementarla al apartado de notas

---
+++ js/actividad.js
@@ -0,0 +1,113 @@
+let banActividad = -1;
+
+$(document).ready(function () {
+    mostrarActividades();
+});
+
+function mostrarActividades() {
+    $.ajax({
+        method: 'get',
+        url: 'http://localhost:8000/verEstudiantes'
+    }).done((response) => {
+        const dataJson = JSON.parse(response);
+        const usuarios = dataJson.data;
+        const table = document.getElementById('actividadesTb');
+        const tbody = table.getElementsByTagName('tbody')[0];
+        let html = '';
+
+        usuarios.forEach(actividad => {
+            html += '<tr>';
+            html += '   <td>' + actividad.id + '</td>';
+            html += '   <td>' + actividad.descripcion + '</td>';
+            html += '   <td>' + actividad.nota + '</td>';
+            html += '   <td>';
+            html += '      <button onclick="modificar(' + actividad.id + ')">Modificar</button>';
+            html += '   </td>';
+            html += '   <td>';
+            html += '      <button onclick="eliminar(' + actividad.id + ')">Eliminar</button>';
+            html += '   </td>';
+            html += '</tr>';
+        });
+
+        tbody.innerHTML = html;
+    }).fail((error) => {
+        console.error(error);
+    });
+}
+
+document.getElementById('registrar').addEventListener('click', () => {
+    banActividad = 0;
+    document.getElementById('tituloModal').innerText = 'Registrar';
+});
+
+document.getElementById('cerrarModal').addEventListener('click', () => {
+    document.getElementById('formularioModal').classList.add('close-modal');
+});
+
+
+document.getElementById('guardar').addEventListener('click', () => {
+    let formulario = document.forms['formularioActividad'];
+    let descripcion = formulario['descripcion'].value;
+    let nota = formulario['nota'].value;
+    let codigoEstudiante = formulario['codigoEstudiante'].value;
+
+    if (descripcion && nota && codigoEstudiante) {
+        if (banActividad == 0) {
+            $.ajax({
+                url: 'http://localhost:8000/agregarEstudiantes',
+                method: 'post',
+                data: {
+                    descripcion: descripcion,
+                    nota: nota,
+                    codigoEstudiante: codigoEstudiante
+                }
+            }).done(response => {
+                const dataJson = JSON.parse(response);
+                const msg = dataJson.data;
+                alert(msg);
+                mostrarActividades();
+                location.reload();
+            });
+        } else if (banActividad == 1) {
+            let formularioModificar = document.forms['formularioActividad'];
+            let descripcionModificar = formularioModificar['descripcion'].value;
+            let notaModificar = formularioModificar['nota'].value;
+            $.ajax({
+                url: 'http://localhost:8000/actualizarEstudiantes/' + id ,
+                method: 'put',
+                data: {
+                    descripcion: descripcionModificar,
+                    nota: notaModificar
+                }
+            }).done(response => {
+                const dataJson = JSON.parse(response);
+                const msg = dataJson.data;
+                alert(msg);
+                mostrarActividades();
+                location.reload();
+            });
+        }
+    } else {
+        alert('No pueden haber campos vacios');
+    }
+});
+
+function modificar(Id) {
+    document.getElementById('tituloModal').innerText = 'Modificar';
+    banActividad = 1;
+    id = Id;
+}
+
+function eliminar(id) {
+    $.ajax({
+        url: 'http://localhost:8000/eliminarEstudiantes/' + id,
+        method: 'delete',
+    }).done(response => {
+        const dataJson = JSON.parse(response);
+        const msg = dataJson.data;
+        alert(msg);
+        mostrarActividades();
+        location.reload();
+
+    });
+}
